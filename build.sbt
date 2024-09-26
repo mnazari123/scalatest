18559b002c8302b799240a03d583f23dc9d1999f
mnazari123 <mnazari@luc.edu>
2024-09-26 12:38:42 -0500

    Initial Changes added

---
+++ build.sbt
@@ -0,0 +1,33 @@
+scalaVersion := "3.3.3" // A Long Term Support version.
+
+// enablePlugins(ScalaNativePlugin)
+
+// set to Debug for compilation details (Info is default)
+// logLevel := Level.Info
+
+// import to add Scala Native options
+// import scala.scalanative.build._
+
+// defaults set with common options shown
+// nativeConfig ~= { c =>
+//   c.withLTO(LTO.none) // thin
+//     .withMode(Mode.debug) // releaseFast
+//     .withGC(GC.immix) // commix
+// }
+
+version := "0.3"
+
+scalaVersion := "3.3.3"
+
+scalacOptions += "@.scalacOptions.txt"
+
+libraryDependencies ++= Seq(
+  "org.scalatest"  %% "scalatest"  % "3.2.19"  % Test,
+  "org.scalacheck" %% "scalacheck" % "1.18.0" % Test,
+  "com.lihaoyi" %% "mainargs" % "0.7.5",
+  "org.log4s" %% "log4s" % "1.10.0",
+  "org.slf4j" % "slf4j-simple" % "2.0.16",
+  "org.knowm.xchart" % "xchart" % "3.8.8"
+)
+
+enablePlugins(JavaAppPackaging)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,170 @@
+// import mainargs.{main, arg, ParserForMethods, Flag}
+// // import org.log4s._
+// // import org.slf4j.LoggerFactory
+
+
+// object Main {
+
+//   // logger.debug("Constructing new instance of MyClass")
+//   // logger.trace(s"New instance's data set:")
+//   @main
+//   def run(@arg(short = 'f', doc = "String to print repeatedly")
+//           foo: String,
+//           @arg(doc = "How many times to print string")
+//           myNum: Int = 2,
+//           @arg(doc = "Example flag, can be passed without any value to become true")
+//           bool: Flag) = {
+//     println(foo * myNum + " " + bool.value)
+//   }
+
+//   def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+//     // ParserForMethods(this).runOrExit(args.toIndexedSeq)
+//     // // private val logger: org.log4s.Logger = Option(org.log4s.getLogger).getOrElse(LoggerFactory.getLogger("Main"))
+//     // // private val logger: org.slf4j.Logger = LoggerFactory.getLogger("Main")
+//     // // internal main method with arguments annotated for parsing
+//     // @main
+//     // def run(
+//     //     @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = 10,
+//     //     @arg(short = 'l', doc = "minimum word length to be considere") minLength: Int = 6,
+//     //     @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = 1000,
+//     //     @arg(short = 's', doc = "number of steps between word cloud updates") everyKSteps: Int = 10,
+//     //     @arg(short = 'f', doc = "minimum frequency for a word to be included in the cloud") minFrequency: Int = 3) =
+
+//     //     logger.debug(f"howMany=$cloudSize minLength=$minLength lastNWords=$windowSize everyKSteps=$everyKSteps minFrequency=$minFrequency")
+//     // println("Hello, world!")
+    
+
+
+// }
+
+
+package testhello
+import scala.collection.mutable
+import scala.io.StdIn
+import scala.sys.process._
+import mainargs.{main, arg, ParserForMethods, Flag}
+import org.log4s._
+import org.slf4j.LoggerFactory
+import org.knowm.xchart.{PieChartBuilder, SwingWrapper}
+
+object Main{
+
+  var cloudSize = 10
+  var minLength = 6
+  var windowSize = 1000
+  var everyksteps = 10
+  var minFrequency = 3
+  // @main
+  // def run(@arg(short = 'f', doc = "String to print repeatedly")
+  //         foo: String,
+  //         @arg(doc = "How many times to print string")
+  //         myNum: Int = 2,
+  //         @arg(doc = "Example flag, can be passed without any value to become true")
+  //         bool: Flag) = {
+  //   println(foo * myNum + " " + bool.value)
+  // }
+
+  @main
+  def run(
+    @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = 10,
+    @arg(short = 'l', doc = "minimum word length to be considere") minLength: Int = 6,
+    @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = 1000,
+    @arg(short = 's', doc = "number of steps between word cloud updates") everyKSteps: Int = 10,
+    @arg(short = 'f', doc = "minimum frequency for a word to be included in the cloud") minFrequency: Int = 3) = {
+      println(f"howMany=$cloudSize minLength=$minLength lastNWords=$windowSize everyKSteps=$everyKSteps minFrequency=$minFrequency")
+  }
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+
+  // def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
+  //   // private val logger: org.log4s.Logger = Option(org.log4s.getLogger).getOrElse(LoggerFactory.getLogger("Main"))
+  //   private val logger: org.slf4j.Logger = LoggerFactory.getLogger("Main")
+  //   // internal main method with arguments annotated for parsing
+  //   @main
+  //   def run(
+  //       @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = 10,
+  //       @arg(short = 'l', doc = "minimum word length to be considere") minLength: Int = 6,
+  //       @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = 1000,
+  //       @arg(short = 's', doc = "number of steps between word cloud updates") everyKSteps: Int = 10,
+  //       @arg(short = 'f', doc = "minimum frequency for a word to be included in the cloud") minFrequency: Int = 3) =
+
+  //       logger.debug(f"howMany=$cloudSize minLength=$minLength lastNWords=$windowSize everyKSteps=$everyKSteps minFrequency=$minFrequency")
+
+  //   // Default values
+    
+
+
+
+    println(s"Cloud size: $cloudSize, Min length: $minLength, Window size: $windowSize")
+
+
+    // Sliding window to track the last N words
+    val window = mutable.Queue[String]()
+
+    // Map to track word frequencies within the window
+    val wordFrequency = mutable.Map[String, Int]().withDefaultValue(0)
+
+    def updateWindow(word: String): Unit = {
+      window.enqueue(word)
+      wordFrequency(word) += 1
+
+      // Remove the oldest word if the window exceeds size
+      if (window.size > windowSize) {
+        val oldWord = window.dequeue()
+        wordFrequency(oldWord) -= 1
+        if (wordFrequency(oldWord) == 0) {
+          wordFrequency.remove(oldWord)
+        }
+      }
+    }
+
+    // Function to print the word cloud
+    def printWordCloud(): Unit = {
+      // val sortedWords = wordFrequency.toSeq.sortBy(-_._2).take(cloudSize)
+
+      // Sort words by frequency and apply filters (minFrequency and cloudSize)
+      val sortedWords = wordFrequency.toSeq
+        .filter(_._2 >= minFrequency) // Only include words with sufficient frequency
+        .sortBy(-_._2)
+        .take(cloudSize)
+
+      val wordCloud = sortedWords.map { case (word, freq) => s"$word: $freq" }.mkString(" ")
+      println(wordCloud)
+
+      // Update chart for dynamic visualization
+      updateChart(sortedWords)
+    }
+
+    // Create the chart and GUI
+    val chart = new PieChartBuilder().width(800).height(600).title("Word Cloud").build()
+    val sw = new SwingWrapper(chart)
+    sw.displayChart()
+    // Function to update the dynamic chart visualization
+    def updateChart(sortedWords: Seq[(String, Int)]): Unit = {
+      chart.getSeriesMap.clear() // Clear the previous chart
+      sortedWords.foreach { case (word, freq) =>
+        chart.addSeries(word, freq)
+      }
+      sw.repaintChart() // Refresh the chart window
+    }
+
+    var steps = 0
+    
+    val lines = scala.io.Source.stdin.getLines()
+    val words = 
+      import scala.language.unsafeNulls
+      lines.flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+"))
+      .map(_.toLowerCase)
+      .filter(_.length >= minLength)
+    words.foreach { word =>
+      updateWindow(word)
+      steps += 1
+      // if (window.size >= windowSize) {
+      //   printWordCloud()
+      // }
+      // Update and print word cloud every `everyksteps`
+      if (window.size >= windowSize && steps % everyksteps == 0) {
+        printWordCloud()
+      }
+    }
+}
